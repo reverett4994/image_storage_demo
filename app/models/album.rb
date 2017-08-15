a7f76d59697e71b0b9cb6eed048141f06d0e2058
Ryan Everett <reverett4994@gmail.com>
2017-08-15 04:49:44 -0400

    tweaked forms and h1s

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,11 @@
 class Album < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :images
+  validates :name, uniqueness: true
+
+  before_save :downcase_name
+
+  def downcase_name
+    self.name.downcase!
+  end
 end
