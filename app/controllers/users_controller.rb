3bbc3890d49a80a3a214b39a53af8e5282b3f81b
Ryan Everett <reverett4994@gmail.com>
2017-08-30 04:39:54 -0400

    started friend dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def show
     @user = User.find(params[:id])
     gon.user=@user.id
-    @public_count=@user.images.where("public LIKE true").count
-    @pfriends_count=@user.images.where("only_friends LIKE true").count
-    @private_count=@user.images.where("private LIKE true").count
+    @public_count=@user.images.where("public LIKE true").count.to_i
+    @friends_count=@user.images.where("only_friends LIKE true").count.to_i
+    @private_count=@user.images.where("private LIKE true").count.to_i
 
   end
 
   def request_friend
-    @friend= User.find(params[:friend])
+    if params[:friend].is_a? Integer
+      @friend= User.find(params[:friend])
+    else
+      @friend= User.where("email LIKE ?",params[:friend])
+      @friend=@friend.last
+    end
     current_user.friend_request(@friend)
 
     respond_to do |format|
