293b87527b8c01bc149dc69db8032d58130ebd0e
Ryan Everett <reverett4994@gmail.com>
2017-08-31 04:41:56 -0400

    sending callback of user after requested friendship

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :images
+  resources :users, param: :email
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
